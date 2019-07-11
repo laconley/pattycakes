5debab9c07f10fbb0699d18bd404bdba858239c7
Lauren Conley <laurenconley789@gmail.com>
2019-07-10 20:36:55 -0600

    add gray class to nav svg when clicked

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let navList = document.querySelector(".nav-ul");
 let navBtn = document.querySelector(".nav-dropdown-svg");
 
 navBtn.addEventListener("click", function(event){
-	navList.classList.toggle("nav-dropdown-open");
+    navList.classList.toggle("nav-dropdown-open");
+    navBtn.classList.toggle("nav-dropdown-svg-gray");
 });
     
